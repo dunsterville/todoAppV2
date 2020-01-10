5cf37353d986d65db5b838684fab13d3e33cc30e
Michael Dunsterville <michael@klientboost.com>
2020-01-09 19:53:19 -0800

    Added react router, material ui, base files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from 'react'
+import Todo from './pages/Todo'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
-  
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Todo />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
